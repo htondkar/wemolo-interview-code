5fdec7d15edc346a0b7c11170d3da91a1117653a
Hamid <hto+github@dixa.com>
2024-09-30 15:30:07 +0200

    add routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,30 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { HomePage } from './pages/Home.page';
+import { TinderView } from './features/parkingMatch/TinderView/view/TinderView';
+import { Summary } from './features/parkingMatch/Summary/view/Summary';
+import { ParkingMatch } from './features/parkingMatch/ParkingMatch';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
   },
+  {
+    path: "/parking-match",
+    element: <ParkingMatch />,
+    children: [
+      {
+        path: "tinder-view",
+        element: <TinderView />,
+      },
+      {
+        path: "summary",
+        element: <Summary />,
+      },
+    ],
+  },
 ]);
+
 
 export function Router() {
   return <RouterProvider router={router} />;
